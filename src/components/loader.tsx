7406f2cab9ee68eb0c5e215e38b6b3912034bf01
Julius Zlioba <julius.zlioba@gmail.com>
2023-07-06 23:52:55 +0200

    v0.2.1

---
+++ src/components/loader.tsx
@@ -0,0 +1,16 @@
+export default function Loader() {
+	return (
+		<div className="mx-auto p-2 text-gray-800 dark:text-gray-200">
+			<svg
+				className="ringIco"
+				viewBox="25 25 50 50"
+				strokeWidth="3"
+				width="50"
+				height="50"
+				stroke="currentColor"
+			>
+				<circle cx="50" cy="50" r="20" />
+			</svg>
+		</div>
+	)
+}
--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,14 +1,20 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
 import type { Database } from '@/types/database.types'
 
 // this middleware refreshes the user's session and must be run
 // for any Server Component route that uses `createServerComponentSupabaseClient`
 export async function middleware(req: NextRequest) {
-	const res = NextResponse.next();
-	const supabase = createMiddlewareClient<Database>({ req, res });
-	await supabase.auth.getSession();
-	return res;
+	const res = NextResponse.next()
+
+	// Create a Supabase client configured to use cookies
+	const supabase = createMiddlewareClient<Database>({ req, res })
+
+	// Refresh session if expired - required for Server Components
+	// https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-session-with-middleware
+	await supabase.auth.getSession()
+
+	return res
 }
